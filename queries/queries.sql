40c8486fcc3bf411751984179cc24c9963902836
isalvama <inesalvans@gmail.com>
2026-03-13 13:54:12 +0100

    Add SQL queries (1 - 4) for product information retrieval

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,14 +1,15 @@
 -- 1. Llista el nom de tots els productes que hi ha en la taula producto.
-
+SELECT DISTINCT nombre FROM producto;
 
 -- 2. Llista els noms i els preus de tots els productes de la taula producto.
-
+SELECT nombre, precio FROM producto;
 
 -- 3. Llista totes les columnes de la taula producto.
-
+SHOW COLUMNS FROM producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
-
+SELECT nombre, productos
+FROM producto;
 
 -- 5. Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (amb un tipus de canvi de 1 € = 1,1 $ i arrodonint el resultat a dues xifres decimals). Utilitza els següents àlies per a les columnes: nom del producte, euros, dòlars.
 
